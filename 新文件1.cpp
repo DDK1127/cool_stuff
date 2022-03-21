53131fff94d1e934446c87e3cb0ce4612f498f66
DDK1127 <derek61003@gmail.com>
2022-03-21 15:56:16 +0800

    first commit

---
+++ 新文件1.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+class node{
+	public:
+	int data;
+	node *next;
+}; 
+
+class list{
+	private:
+	 	node *not_use;
+	 	node *frist = NULL;
+	public:
+		void push(int x);
+		void pop();
+		void display();
+};
+
+void list::push(int x){
+	node *newnode = new node;
+	newnode->data = x;
+	newnode->next = frist;
+	frist = newnode;
+} 
+
+void list::pop(){
+	not_use = frist;
+	frist = frist->next;
+	delete not_use;
+}
+void list::display(){
+	node *current = frist;
+	
+	while(current != NULL){
+		cout << current->data << endl;
+		current = current->next;
+	}
+} 
+int main(){
+	list A1;
+	A1.push(6);
+	A1.push(99);
+	A1.push(54);
+	A1.push(1);
+	A1.pop();
+	A1.display();
+	return 0;
+}
